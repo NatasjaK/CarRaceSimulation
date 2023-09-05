6da7a653bc02879fd54dbd538c5dfe8c72b1f561
NatasjaK <natasja.kauppi@gmail.com>
2023-09-05 13:42:06 +0200

    Simulate car race with threading

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRaceSimulation
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public double Position { get; set; }
+        public int Speed { get; set; }
+
+        public Car(string name, int speed)
+        {
+            Name = name;
+            Position = 0;
+            Speed = 120; // Grundhastighet (120 km/h)
+        }
+    }
+}
